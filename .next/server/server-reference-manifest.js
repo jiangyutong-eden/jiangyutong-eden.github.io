e9bfc919ceeeac3bfd7634166d45e734b0b6d504
jiangyytong126-arch <jiangyytong126@gmail.com>
2026-03-16 15:08:08 +0800

    Update awards and publications

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dIpCH606W4PWdp/EjSxDhRfqmyWRAfLAeVJ8mDtFmbU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qxiG9Me3FQZH6wTUtm2QyV/E/g04gAVs/e//EWJDIoA=\"\n}"
